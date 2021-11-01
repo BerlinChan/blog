636020bf9dbaf03f601d588662202d1761899032
Berlin Chan <berlinchancom@gmail.com>
2021-11-01 16:45:02 +0800

    fix scrollTop Fab position

--- src/components/Layout/ScrollTop/ScrollTop.js
+++ src/components/Layout/ScrollTop/ScrollTop.js
@@ -1,44 +1,37 @@
-import React from 'react'
-import { styled } from '@mui/material/styles';
-import useScrollTrigger from '@mui/material/useScrollTrigger'
-import Zoom from '@mui/material/Zoom'
-import Fab from '@mui/material/Fab'
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
-
-const PREFIX = 'ScrollTop';
-
-const classes = {
-  scrollToTop: `${PREFIX}-scrollToTop`
-};
-
-const StyledZoom = styled(Zoom)({
-  [`& .${classes.scrollToTop}`]: {
-    position: 'fixed',
-    bottom: 16,
-    right: 16,
-  },
-});
+import React from "react";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+import Zoom from "@mui/material/Zoom";
+import Fab from "@mui/material/Fab";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
 const ScrollTop = (props) => {
-
   const trigger = useScrollTrigger({
     disableHysteresis: true,
     threshold: 100,
-  })
+  });
 
-  const handleClick = event => {
-    window.scrollTo({ top: 0, behavior: 'smooth', })
-  }
+  const handleClick = (event) => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
 
   return (
-    <StyledZoom in={trigger}>
-      <div onClick={handleClick} role="presentation" className={classes.scrollToTop}>
-        <Fab color="secondary" size="medium" aria-label="scroll back to top">
-          <KeyboardArrowUpIcon/>
+    <Zoom in={trigger}>
+      <div onClick={handleClick} role="presentation">
+        <Fab
+          sx={{
+            position: "fixed",
+            bottom: 16,
+            right: 16,
+          }}
+          color="secondary"
+          size="medium"
+          aria-label="scroll back to top"
+        >
+          <KeyboardArrowUpIcon />
         </Fab>
       </div>
-    </StyledZoom>
+    </Zoom>
   );
-}
+};
 
-export default ScrollTop
+export default ScrollTop;
