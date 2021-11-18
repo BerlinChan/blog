7827f84faa467828e05dc838761816fa1d39a27a
Berlin Chan <berlinchancom@gmail.com>
2021-11-18 10:42:39 +0800

    category style

--- src/components/Sidebar/Categories/Categories.js
+++ src/components/Sidebar/Categories/Categories.js
@@ -1,66 +1,60 @@
-import React from 'react'
-import { styled } from '@mui/material/styles';
-import { Link as GatsbyLink } from 'gatsby'
-import Link from '@mui/material/Link'
-import Typography from '@mui/material/Typography'
-import { useCategoriesList } from '../../../hooks'
-import kebabCase from 'lodash/kebabCase'
-import Box from '@mui/material/Box'
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz'
-import IconButton from '@mui/material/IconButton'
-
-const PREFIX = 'Categories';
-
-const classes = {
-  title: `${PREFIX}-title`,
-  link: `${PREFIX}-link`,
-  activeLink: `${PREFIX}-activeLink`
-};
-
-const StyledBox = styled(Box)((
-  {
-    theme
-  }
-) => ({
-  [`& .${classes.title}`]: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    padding: theme.spacing(.5, 1),
-    backgroundColor: theme.palette.background.paper,
-  },
-
-  [`& .${classes.link}`]: {
-    padding: theme.spacing(1),
-    '&:hover': {
-      backgroundColor: theme.palette.grey[200],
-    },
-  },
-
-  [`& .${classes.activeLink}`]: {
-    color: theme.palette.action.active,
-  }
-}));
+import React from "react";
+import { useTheme, emphasize } from "@mui/material/styles";
+import { Link as GatsbyLink } from "gatsby";
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
+import { useCategoriesList } from "../../../hooks";
+import kebabCase from "lodash/kebabCase";
+import Box from "@mui/material/Box";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import IconButton from "@mui/material/IconButton";
 
 const Categories = (props) => {
-
-  const categories = useCategoriesList()
+  const theme = useTheme();
+  const categories = useCategoriesList();
 
   return (
-    <StyledBox {...props}>
-      <Box className={classes.title}>
+    <Box {...props}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          padding: theme.spacing(0.5, 1),
+          backgroundColor: theme.palette.background.paper,
+        }}
+      >
         <Typography variant="subtitle1">分类</Typography>
-        <IconButton component={GatsbyLink} to={'/categories'} aria-label="more" size="small" title={'全部'}>
-          <MoreHorizIcon/>
+        <IconButton
+          component={GatsbyLink}
+          to={"/categories"}
+          aria-label="more"
+          size="small"
+          title={"全部"}
+        >
+          <MoreHorizIcon />
         </IconButton>
       </Box>
-      {categories.map((category, index) =>
-        <Link component={GatsbyLink} to={`/category/${kebabCase(category.fieldValue)}/`}
-              display="block" variant="body1" noWrap key={index}
-              className={classes.link} activeClassName={classes.activeLink}>
+      {categories.map((category, index) => (
+        <Link
+          component={GatsbyLink}
+          to={`/category/${kebabCase(category.fieldValue)}/`}
+          display="block"
+          variant="body1"
+          underline="none"
+          noWrap
+          key={index}
+          sx={{
+            padding: theme.spacing(1),
+            ":hover": {
+              backgroundColor: emphasize(theme.palette.background.default, 0.1),
+            },
+          }}
+        >
           {`${category.fieldValue} (${category.totalCount})`}
-        </Link>)}
-    </StyledBox>
+        </Link>
+      ))}
+    </Box>
   );
-}
+};
 
-export default Categories
+export default Categories;
